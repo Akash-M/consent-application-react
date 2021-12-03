58268a8115aafc7c478c7cdb2c35ac549522ec95
Akash-M <akash.manjunath@gmail.com>
2021-12-03 09:42:29 +0100

    docs: add tsdocs to mock api calls

--- packages/frontend/lib-api/src/consent.ts
+++ packages/frontend/lib-api/src/consent.ts
@@ -4,10 +4,21 @@ import { consentListFixtures } from '$/fixtures';
 
 let consents = consentListFixtures;
 
+/**
+ * Mock Api call to fetch list of available consents.
+ *
+ * @return Promise<Consent.Detail[]>
+ */
 export async function getConsents(): Promise<Consent.Detail[]> {
   return await Promise.resolve(consents);
 }
 
+/**
+ * Mock Api call to add newly created consent to existing consents.
+ *
+ * @param consent: New consent to be added.
+ * @return Promise<Consent.Detail>
+ */
 export async function postConsent(
   consent: Consent.NewEntry,
 ): Promise<Consent.Detail> {
