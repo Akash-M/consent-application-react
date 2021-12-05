7b0895a5c975fd78d1aab6350d0171f766570f41
Akash-M <akash.manjunath@gmail.com>
2021-12-05 11:40:38 +0100

    refactor: move email-regex to lib-utils

--- packages/frontend/lib-utils/src/email-regex.ts
+++ packages/frontend/lib-utils/src/email-regex.ts
@@ -1,9 +1,1 @@
-// TODO: simplify regex.
-/*
- * deprecated.
- */
-export const validateEmail = (email: string): boolean => {
-  const re =
-    /^(([^\s"(),.:;<>@[\\\]]+(\.[^\s"(),.:;<>@[\\\]]+)*)|(".+"))@((\[(?:\d{1,3}\.){3}\d{1,3}])|(([\dA-Za-z-]+\.)+[A-Za-z]{2,}))$/;
-  return re.test(email.toLowerCase());
-};
+export const EMAIL_REGEX = /^\S+@\S+\.\S+$/;
